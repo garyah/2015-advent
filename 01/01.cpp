639d8961e332d3069172722a6d1b4511b86fa0e8
Gary Hildebrand <gary@Garys-MacBook-Air.local>
2015-12-07 00:36:12 -0800

    Main module reads lines of text from standard input, until first empty line, EOF, or error.  Max length of lines limited to 80 characters.

--- 01/01.cpp
+++ 01/01.cpp
@@ -1,6 +1,16 @@
+#define MAX_LINE_SIZE 80 + 1
+
 #include <stdio.h>
 
 int main()
 {
+	char line[MAX_LINE_SIZE] = {0};
+	while (fgets(line, MAX_LINE_SIZE, stdin) != NULL
+		   && line[0] != '\r'
+		   && line[0] != '\n')
+	{
+		// TODO
+		;
+	}
 	return 0;
 }
