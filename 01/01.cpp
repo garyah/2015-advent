8520a1227ab7c792b8a9f10f73b15cc7210b1b07
Gary Hildebrand <garyah@users.noreply.github.com>
2015-12-08 10:23:04 -0800

    Blank lines no longer end program, and EOF with no line delimiter now processes line.

--- 01/01.cpp
+++ 01/01.cpp
@@ -1,6 +1,7 @@
 #define MAX_LINE_SIZE 80000 + 1
 
 #include <stdio.h>
+#include <string.h>
 
 #include "ParensParser.hpp"
 
@@ -9,11 +10,20 @@ using namespace Advent2015;
 int main()
 {
 	char line[MAX_LINE_SIZE] = {0};
-	while (fgets(line, MAX_LINE_SIZE, stdin) != NULL
-		   && line[0] != '\r'
-		   && line[0] != '\n')
+	while (!feof(stdin)
+		   && (fgets(line, MAX_LINE_SIZE, stdin) != NULL
+		       || feof(stdin)))
 	{
-		// TODO
+		if (line[0] == '\r' || line[0] == '\n')
+		{
+			continue;
+		}
+		char *pEnd = line + strlen(line) - 1;
+		if (*pEnd == '\r' || *pEnd == '\n')
+		{
+			*pEnd = 0;
+		}
+
 		ParensParser parser;
 		int result = parser.parse(line);
 		(void)printf("%d\n", result);
