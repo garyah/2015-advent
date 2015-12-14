87e5a7f08abf47c81e7091fa151ec82a4d76605f
Gary Hildebrand <garyah@users.noreply.github.com>
2015-12-13 22:45:36 -0800

    Add files for day 2 puzzle 2.

--- 02/run_tests.sh
+++ 02/run_tests.sh
@@ -1,2 +1,4 @@
 cat tests_input.txt | ./02a > tests_output_a.txt
 diff tests_output_a_expected.txt tests_output_a.txt
+cat tests_input.txt | ./02b > tests_output_b.txt
+diff tests_output_b_expected.txt tests_output_b.txt
