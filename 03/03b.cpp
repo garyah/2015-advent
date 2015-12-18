fad9520fdbe86cdfa1b2d334572773177adc81f0
Gary Hildebrand <garyah@users.noreply.github.com>
2015-12-17 20:40:29 -0800

    Make room for day 3 puzzle 2.
    Add to previous commit.

---
+++ 03/03b.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "../common/InputReader.hpp"
+#include "MovesParser.hpp"
+
+using namespace Common;
+using namespace Advent2015;
+
+int main()
+{
+	char *line = NULL;
+	while ((line = InputReader::ReadLine()) != NULL)
+	{
+		MovesParser parser;
+		parser.parse(line);
+		(void)printf("%zu\n", parser.getNumHousesGifted());
+	}
+	return 0;
+}
--- 03/run_tests.sh
+++ 03/run_tests.sh
@@ -1,2 +1,5 @@
-cat tests_input.txt | ./03a > tests_output.txt
-diff tests_output_expected.txt tests_output.txt
+cat tests_input_a.txt | ./03a > tests_output_a.txt
+diff tests_output_a_expected.txt tests_output_a.txt
+cat tests_input_b.txt | ./03b > tests_output_b.txt
+diff tests_output_b_expected.txt tests_output_b.txt
+
