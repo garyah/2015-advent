8e96713e902f81c3a29e6468d9407eb2bffc9dc7
Gary Hildebrand <garyah@users.noreply.github.com>
2015-12-18 23:28:28 -0800

    Start day 4 puzzle 2.

---
+++ 04/04b.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "../common/InputReader.hpp"
+#include "HashInputGenerator.hpp"
+
+using namespace Common;
+using namespace Advent2015;
+
+int main()
+{
+	char *line = NULL;
+	while ((line = InputReader::ReadLine()) != NULL)
+	{
+		HashInputGenerator generator;
+		generator.generate(line);
+		(void)printf("%u\n", generator.getHashInputDecimal());
+		(void)fprintf(stderr, "%s\n", generator.getHashOutputString());
+	}
+	return 0;
+}
--- 04/build.sh
+++ 04/build.sh
@@ -1,1 +1,2 @@
 clang -Wno-deprecated-declarations -lcrypto -lstdc++ 04a.cpp -o 04a
+clang -Wno-deprecated-declarations -lcrypto -lstdc++ 04b.cpp -o 04b
