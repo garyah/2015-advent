f40b7c7ed63255f032fbcf0c9809f162a6511eeb
Gary Hildebrand <garyah@users.noreply.github.com>
2015-12-20 11:25:52 -0800

    Create files for day 5 puzzle 2.

---
+++ 05/05b.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "../common/InputReader.hpp"
+#include "NiceCounter.hpp"
+
+using namespace Common;
+using namespace Advent2015;
+
+int main()
+{
+	NiceCounter counter;
+
+	char *line = NULL;
+	while ((line = InputReader::ReadLine()) != NULL)
+	{
+		counter.addString(line);
+	}
+
+	(void)printf("%u\n", counter.getNiceCount());
+	return 0;
+}
--- 05/run_tests.sh
+++ 05/run_tests.sh
@@ -1,2 +1,5 @@
 cat tests_input_a.txt | ./05a > tests_output_a.txt
 diff tests_output_a_expected.txt tests_output_a.txt
+cat tests_input_b.txt | ./05b > tests_output_b.txt
+diff tests_output_b_expected.txt tests_output_b.txt
+
